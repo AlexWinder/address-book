66f5d42c00f594e46554c2806ece54cd8687efd9
Alex Winder <contact@alexwinder.uk>
2018-04-27 18:45:51 +0100

    Defined PAGENAME and PAGELINKS in navigation bar

--- includes/layout.navigation.inc.php
+++ includes/layout.navigation.inc.php
@@ -10,14 +10,14 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="index.php">Address Book</a>
+					<?php //<a class="navbar-brand" href="index.php">Address Book</a> ?>
 				</div>
 				<div id="navbar" class="navbar-collapse collapse">
 					<ul class="nav navbar-nav">
-						<li class="active"><a href="index.php"><i class="fa fa-address-book" aria-hidden="true"></i> Home</a></li>
-						<li><a href="users.php"><i class="fa fa-users" aria-hidden="true"></i> Users</a></li>
-						<li><a href="logs.php"><i class="fa fa-list" aria-hidden="true"></i> Logs</a></li>
-						<li><a href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
+						<li <?php if($page_name == PAGENAME_INDEX) echo 'class="active"'; ?>><a href="<?php echo PAGELINK_INDEX; ?>"><i class="fa fa-address-book" aria-hidden="true"></i> <?php echo PAGENAME_INDEX; ?></a></li>
+						<li <?php if($page_name == PAGENAME_USERS) echo 'class="active"'; ?>><a href="<?php echo PAGELINK_USERS; ?>"><i class="fa fa-users" aria-hidden="true"></i> <?php echo PAGENAME_USERS; ?></a></li>
+						<li <?php if($page_name == PAGENAME_LOGS) echo 'class="active"'; ?>><a href="<?php echo PAGELINK_LOGS; ?>"><i class="fa fa-list" aria-hidden="true"></i> <?php echo PAGENAME_LOGS; ?></a></li>
+						<li <?php if($page_name == PAGENAME_LOGOUT) echo 'class="active"'; ?>><a href="<?php echo PAGELINK_LOGOUT; ?>"><i class="fa fa-sign-out" aria-hidden="true"></i> <?php echo PAGENAME_LOGOUT; ?></a></li>
 					</ul>
 				</div><!--/.nav-collapse -->
 			</div>
@@ -27,4 +27,3 @@
 			<h2><?php echo $page_name; ?></h2>
 			
 			<hr/>
-
