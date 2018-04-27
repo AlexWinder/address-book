dbef8af5f7d8eeb32a93136afae8c0238c3f1f56
Alex Winder <contact@alexwinder.uk>
2018-04-27 19:51:16 +0100

    Added DB class to be used to obtain an instance of the database using PDO

---
+++ includes/class.db.inc.php
@@ -0,0 +1,30 @@
+<?php
+	// This class is for returning an instance of the DB with a getInstance() method call
+	class DB {
+		
+		// Hold any instantiated PDO object for DB in an $instance as part of a singleton call
+		// Set default to null
+		protected static $instance = null;
+		
+		protected function __construct() {}
+		
+		// Database static method for obtaining Singleton PDO call
+		public static function get_instance() {
+			// If $instance hasn't been set
+			if(empty(self::$instance)) {
+				// Attempt to create a new PDO connection
+				try {
+					// Set $instance to a new PDO, as currently not set
+					self::$instance = new PDO(DB_TYPE.":host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASS);
+				} catch(PDOException $error) {
+					// If an error has been found
+					echo $error->getMessage();
+				}
+			}
+			// Return the Singleton $instance
+			return self::$instance;
+		}
+		
+	}; // Close class Database
+	
+// EOF
