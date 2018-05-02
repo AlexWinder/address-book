e82425e65aeb74f1aecfdb030f83cfe5874fb745
Alex Winder <contact@alexwinder.uk>
2018-05-02 19:56:42 +0100

    Added find_all and find_id methods on Contact class

--- includes/class.contact.inc.php
+++ includes/class.contact.inc.php
@@ -1,6 +1,65 @@
 <?php
 	// This class is for manipulating data relating to Contacts
 	class Contact {
+		// Variable to hold a DB instance
+		private $db;
+		
+		public $all = null, // Variable used to hold all 
+			   $single = null; // Variable used to hold details of single contact ID
+		
+		// Constructor
+		function __construct($id = null) {
+			// Set the $db with an instance of the database
+			$this->db = DB::get_instance();
+			
+			// If an $id is sent through then return the contact associated with the ID
+			// Check if an $id has been sent
+			if($id) {
+				// $id has been sent, find only that contact
+				$this->find_id($id);
+			} else {
+				// No $id has been sent, find all contacts
+				$this->find_all();
+			}
+		}
+		
+		// Class to find all contacts in the database
+		public function find_all() {
+			// Return all 
+			return $this->all = $this->db->query('SELECT * FROM contacts', PDO::FETCH_ASSOC);
+		}
+		
+		// Class to find specific contact in the database
+		public function find_id($id = null) {
+			// Check if $id has been sent
+			if($id) {
+				// Begin prepared statement to find single ID in database
+				$sql = '
+					SELECT * FROM contacts 
+					WHERE contact_id = :contact_id
+				';
+				$stmt = $this->db->prepare($sql);
+				
+				// Pass in the $id into the prepared statement and execute
+				$stmt->bindParam(':contact_id', $id);
+				$stmt->execute();
+				
+				// Fetch the results from the prepared statement
+				$result = $stmt->fetch();
+				
+				// Check if a contact could be found
+				if($result) {
+					// Contact found, return all of the details
+					return $this->single = $result;
+				} else {
+					// Contact not found, return false
+					return false;
+				}
+			} else {
+				// $id not sent, return false
+				return false;
+			}
+		}
 		
 	}; // Close class Contact
 	
