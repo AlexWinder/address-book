42321d749a96aeda31ba9064e874b889a80476da
Alex Winder <contact@alexwinder.uk>
2018-05-09 21:45:05 +0100

    Added methods to set user as logged in and remove any authentication

--- includes/class.user.inc.php
+++ includes/class.user.inc.php
@@ -4,6 +4,10 @@
 		
 		private $db = null; // Used to store an instance of the database
 		
+		public 	$authenticated = false, // Used to know if the user is authenticated or not
+				$details = false, // Used to store all details about the user in an array
+				$name = false; // The users name as retrieved from the database
+		
 		// Constructor
 		public function __construct() {
 			// Obtain an instance of the database
@@ -42,7 +46,52 @@
 				return false;
 			} // Close if(isset($username) && isset($password) && !empty($username) && !empty($password))
 		}
-	
+		
+		// Used to remove any authenticated user 
+		public function remove_authenticated() {
+			// Call in the $session as required
+			global $session;
+			
+			// Remove Session values first
+			$session->remove('authenticated_user');
+			$session->remove('authenticated_user_ip');
+			$session->remove('authenticated_user_agent');
+			$session->remove('authenticated_user_time');
+			$session->remove('authenticated_user_id');
+			
+			// Remove any set properties in this instance
+			$this->authenticated = false;
+			$this->details = false;
+			$this->name = false;
+		}
+		
+		// Used to set various session values which will be used for checking that the user is logged in correctly
+		// Called after a user has successfully been logged in
+		public function set_logged_in($user = null) {
+			// Call in the $session as required
+			global $session; 
+			
+			// Check that the relevant information has been sent
+			if(isset($user) && !empty($user)) {
+				// Remove any pre-existing settings
+				$this->remove_authenticated();
+				
+				// Set value to notify that the user has been authenticated
+				$session->set('authenticated_user', '1');
+				$this->authenticated = true;
+				
+				// Set details gathered about the session from when the user was authenticated
+				// Remove any pre-existing details
+				$session->set('authenticated_user_ip', $_SERVER['REMOTE_ADDR']);
+				$session->set('authenticated_user_agent', $_SERVER['HTTP_USER_AGENT']);
+				$session->set('authenticated_user_time', time());
+				
+				// Set details gathered about the user
+				$session->set('authenticated_user_id', $user['user_id']);
+				$session->set('authenticated_user_username', $user['username']);
+			}
+		}
+		
 		// Used for verifying that users supplied password matches
 		private function password_check($password, $existing_hash) {
 			// Exisiting hash contains format and salt at start
