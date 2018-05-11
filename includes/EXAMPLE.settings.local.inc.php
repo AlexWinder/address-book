30118bfe04475cb48d1882073cc39e6f9734c642
Alex Winder <contact@alexwinder.uk>
2018-05-11 22:19:23 +0100

    Added example local settings file

---
+++ includes/EXAMPLE.settings.local.inc.php
@@ -0,0 +1,5 @@
+<?php	
+	defined("DB_SERVER")    	?	null	:	define("DB_SERVER", "127.0.0.1");
+	defined("DB_USER")			?	null	:	define("DB_USER", "root");
+	defined("DB_PASS")			?	null	:	define("DB_PASS", "");
+	defined("DB_NAME")			?	null	:	define("DB_NAME", "address_book");
