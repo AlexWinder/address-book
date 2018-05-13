4dcfca614eab00b8e9e0e9d67edba3506a4d10a7
Alex Winder <contact@alexwinder.uk>
2018-05-13 17:15:43 +0100

    Added constructor and find_all method

--- includes/class.log.inc.php
+++ includes/class.log.inc.php
@@ -2,7 +2,19 @@
 	// This class is used for logging user actions to the database in the logs table
 	class Log {
 		
+		private $db = null; // Used to store an instance of the database
 		
+		// Constructor
+		public function __construct() {
+			// Obtain an instance of the database
+			$this->db = DB::get_instance();
+		}
+		
+		// Find all logs from the database
+		public function find_all() {
+			// Return all 
+			return $this->all = $this->db->query('SELECT * FROM logs', PDO::FETCH_ASSOC);
+		}
 		
 	}; // Close class Log
 // EOF
