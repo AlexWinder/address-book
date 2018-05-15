c19552d7924d2b49f34adf3c77dc6a409002c101
Alex Winder <contact@alexwinder.uk>
2018-05-15 22:38:19 +0100

    Replaced log_action function with Log class with constructor

--- html/delete-user.php
+++ html/delete-user.php
@@ -52,21 +52,26 @@
 								// Set session message
 								$session->message_alert($notification["user"]["delete"]["success"], "success");
 								// Log action of add entry success, with user deleted
-								log_action("delete_success", "User " . $user_full_name_with_username . " was deleted.");
+								// Create new Log instance, and log the action to the database
+								$log = new Log('user_delete_success', 'User - ' . $user_full_name_with_username);
+								// Redirect the user
 								redirect_to("users.php");
 							} else {
 								// User failed to be deleted
 								// Set session message
 								$session->message_alert($notification["user"]["delete"]["failure"], "danger");
 								// Log action of database entry failing
-								log_action("delete_failed", $logging["database"]["failure"]);
+								// Create new Log instance, and log the action to the database
+								$log = new Log('user_delete_failed', 'database');
 							};
 						} else {
 							// User has attempted to delete their own account from the system
 							// Set session message
 							$session->message_alert($notification["user"]["delete"]["self"], "danger");
 							// Log action of database entry failing
-							log_action("delete_failed", "User attempted to delete their own user account.");
+							// Create new Log instance, and log the action to the database
+							$log = new Log('user_delete_failed', 'User attempted to delete their own user account.');
+							// Redirect the user
 							redirect_to("users.php");
 						};
 					
@@ -81,7 +86,9 @@
 					// Set a failure session message and redirect them to view the user list
 					$session->message_alert($validation["field_required"]["user"]["confirm_delete"], "danger");
 					// Log action of failing to confirm delete
-					log_action("delete_failed", "User did not confirm that they wanted to delete the user.");
+					// Create new Log instance, and log the action to the database
+					$log = new Log('user_delete_failed', 'User did not confirm that they wanted to delete the user.');
+					// Redirect the user
 					redirect_to("users.php");
 				};
 			}; // User has not submitted the form - do nothing
