9996e2161be4aa1a77b6acf304beab49a93b8e0c
Alex Winder <contact@alexwinder.uk>
2018-05-19 21:20:12 +0100

    Created add/delete/update API pages

---
+++ html/add-api.php
@@ -0,0 +1,29 @@
+<?php
+	// Require relevent information for settings.config.inc.php, including functions and database access
+	require_once("../includes/settings.config.inc.php");
+	
+	// Set $page_name so that the title of each page is correct
+	$page_name = PAGENAME_API;
+	// Set $subpage_name as this page isn't the main section
+	$subpage_name = PAGENAME_APIADD;
+	
+	// Check if $user is authenticated
+	if(!$user->authenticated) {
+		$user->logout('not_authenticated');
+	}; // Close if(!$user->authenticated)
+	
+	// Require head content in the page
+	require_once("../includes/layout.head.inc.php");
+	// Requre navigation content in the page
+	require_once("../includes/layout.navigation.inc.php");
+?>
+	
+			<!-- CONTENT -->
+			<?php $session->output_message(); ?>
+			
+			<!-- /CONTENT -->
+
+<?php
+	// Requre footer content in the page, including any relevant scripts
+	require_once("../includes/layout.footer.inc.php");
+?>
