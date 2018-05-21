5f8d3f4ed216205bdad33b8b54afe994a38f74cb
Alex Winder <contact@alexwinder.uk>
2018-05-21 15:13:39 +0100

    Check if GET value sent

--- html/delete-api.php
+++ html/delete-api.php
@@ -10,6 +10,20 @@
 		$user->logout('not_authenticated');
 	}; // Close if(!$user->authenticated)
 
+	// If the value of i in GET exists
+	if(isset($_GET["i"])) {
+	
+	} else {
+		// Value of i in GET doesn't exist, send session message and redirect
+		$session->message_alert($notification["api"]["delete"]["not_found"], "danger");
+		// Set $page_name so that the title of each page is correct - GET value not correct
+		$subpage_name = 'Invalid GET Value - ' . PAGENAME_APIDELETE;
+		// Create new Log instance, and log the action to the database
+		$log = new Log('not_found');
+		// Redirect the user
+		Redirect::to(PAGELINK_INDEX);
+	};
+
 	// Require head content in the page
 	require_once("../includes/layout.head.inc.php");
 	// Requre navigation content in the page
