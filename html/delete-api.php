d8c5b0cdf6dc1fa587f128fd7640dce21d583f60
Alex Winder <contact@alexwinder.uk>
2018-05-21 15:21:38 +0100

    Check if API token could be found

--- html/delete-api.php
+++ html/delete-api.php
@@ -12,7 +12,30 @@
 
 	// If the value of i in GET exists
 	if(isset($_GET["i"])) {
-		
+		// Find API token in database
+		$api = new API();
+		$api = $api->find_id($_GET['i']);
+
+		// If a API token is found in the database
+		if($api) {
+			// Set page name as API token could be found
+			$subpage_name = $api['api_id'] . ' - ' . PAGENAME_APIDELETE;
+			
+			// Obtain a CSRF token to be used to prevent CSRF - this is stored in the $_SESSION
+			$csrf_token = CSRF::get_token();
+
+		} else {
+			// API token could not be found in the database
+			// Send session message and redirect
+			$session->message_alert($notification["api"]["delete"]["not_found"], "danger");
+			// Set $subpage_name so that the title of each page is correct - contact couldn't be found
+			$subpage_name = 'API Token Not Found - ' . PAGENAME_APIDELETE;
+			// Create new Log instance, and log the action to the database
+			$log = new Log('not_found');
+			// Redirect the user
+			Redirect::to(PAGELINK_API);
+		};
+
 	} else {
 		// Value of i in GET doesn't exist, send session message and redirect
 		$session->message_alert($notification["api"]["delete"]["not_found"], "danger");
