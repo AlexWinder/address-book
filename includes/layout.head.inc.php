b991240a77ae38beba7e4c0265367957bc2ff836
Alex Winder <contact@alexwinder.uk>
2018-05-21 21:32:17 +0100

    Call third party files locally

--- includes/layout.head.inc.php
+++ includes/layout.head.inc.php
@@ -4,14 +4,14 @@
 		<title><?php echo page_name(); ?></title>
 		
 		<!-- jQuery -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="assets/jQuery/3.2.1/jquery.min.js"></script>
 <?php
 		if(isset($datatables_required) && $datatables_required == 1) {
 			$datatables_source = <<<FILEDOC
 		
 		<!-- DataTables -->
-		<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.css">
-		<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.15/js/jquery.dataTables.js"></script>
+		<link rel="stylesheet" type="text/css" href="assets/DataTables/1.10.15/css/jquery.dataTables.css">
+		<script type="text/javascript" charset="utf8" src="assets/DataTables/1.10.15/js/jquery.dataTables.js"></script>
 		
 FILEDOC;
 			echo $datatables_source;
@@ -19,15 +19,15 @@ FILEDOC;
 ?>
 		
 		<!-- Twitter Bootstrap -->
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- Minified CSS -->
+		<link rel="stylesheet" href="assets/bootstrap/3.3.7/css/bootstrap.min.css">
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="assets/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+		<!-- Minified JavaScript for Bootstrap -->
+		<script src="assets/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		
 		<!-- Font Awesome -->
-		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+		<link href="assets/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 		
 		<!-- Main CSS -->
 		<link rel="stylesheet" href="css/main.css?v=<?php echo time(); ?>">
