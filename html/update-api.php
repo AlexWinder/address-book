74c662954505d49227c525eb25050d2df2416f05
Alex Winder <contact@alexwinder.uk>
2018-05-21 16:25:43 +0100

    Check if GET value sent

--- html/update-api.php
+++ html/update-api.php
@@ -10,6 +10,23 @@
 		$user->logout('not_authenticated');
 	}; // Close if(!$user->authenticated)
 
+	// If the value of i in GET exists
+	if(isset($_GET["i"])) {
+		// Find API token in database
+		$api = new API();
+		$api->find_id($_GET['i']);
+	
+	} else {
+		// Value of i in GET doesn't exist, send session message and redirect
+		$session->message_alert($notification["api"]["update"]["not_found"], "danger");
+		// Set $page_name so that the title of each page is correct - GET value not correct
+		$subpage_name = 'Invalid GET Value - ' . PAGENAME_APIUPDATE;
+		// Create new Log instance, and log the action to the database
+		$log = new Log('not_found');
+		// Redirect the user
+		Redirect::to(PAGELINK_API);
+	};
+
 	// Require head content in the page
 	require_once("../includes/layout.head.inc.php");
 	// Requre navigation content in the page
