e778b260e492e455a0be6aeedea566f64de8663f
Alex Winder <contact@alexwinder.uk>
2018-05-21 16:45:52 +0100

    Updated HTML form, was copy/pasted incorrectly

--- html/update-api.php
+++ html/update-api.php
@@ -59,19 +59,41 @@
 			
 			<form class="form-horizontal" action="" method="post">
 
-				<div class="checkbox">
-					<label>
-						<input type="checkbox" name="confirm_delete"> Yes, I am sure that I want to <strong>permanently delete</strong> API token <?php echo $api->token; ?>
-					</label>
+				<div class="form-group">
+					<label class="col-sm-2 control-label">API Token</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" value="<?php echo htmlentities($api->token); ?>" disabled>
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label class="col-sm-2 control-label">Cosmetic Name*</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" name="cosmetic_name" placeholder="Give the API token a cosmetic name to allow you to easily identify it (Optional)" <?php if(isset($_POST["cosmetic_name"])){ echo "value=\"" . htmlentities($_POST["cosmetic_name"]) . "\""; } elseif(isset($api->name) && !empty($api->name)) { echo "value=\"" . htmlentities($api->name) . "\""; }; ?>>
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label class="col-sm-2 control-label">IP Address**</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" name="ip_address" value="<?php if(isset($api->ip) && !empty($api->ip)) { echo htmlentities($api->ip); } else { echo "Accessible from any IP"; } ?>" disabled>
+						<small>You are unable to update the IP address associated with the API token. To allow access from another IP address you need to <a href="<?php echo PAGELINK_APIADD; ?>">create a new API token</a>.</small>
+					</div>
 				</div>
 				
 				<input type="hidden" name="csrf_token" value="<?php echo htmlentities($csrf_token); ?>"/>
 
 				<hr>
 
-				<div >
-					<button type="submit" name="submit" value="submit" class="btn btn-danger">Delete API Token</button>
-				</div>
+				<p>* = Optional field</p>
+				<p>** = If no IP address is specified, then the API token will be able to be used from any IP address. You are unable to update the IP address once an API token has been created. For security purposes you will need to create a new API token if you wish to use a different IP address.</p>
+				
+
+				<div class="form-group">
+					<div class="col-sm-offset-2 col-sm-10">
+						<button type="submit" name="submit" value="submit" class="btn btn-default">Submit</button>
+					</div>
+				</div
 			</form>
 
 			<!-- /CONTENT -->
