53a8ba93596dee7c1a8b2b2e3807ac29ab7d9aca
Sal Zaydon <sal@zaydon.email>
2025-04-16 14:05:10 -0500

    Moving assets to be local rather than using CDN

--- includes/layout.head.inc.php
+++ includes/layout.head.inc.php
@@ -6,44 +6,33 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
 		<!-- jQuery -->
-		<script src="assets/jQuery/3.5.0/jquery.min.js"></script>
+		<script src="assets/libraries/jQuery/3.6.4/jquery-3.6.4.min.js"></script>
 <?php
 		if(isset($datatables_required) && $datatables_required == 1) {
 			$datatables_source = <<<FILEDOC
 		
 		<!-- DataTables -->
-		<link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css">
-		<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
+		<link rel="stylesheet" href="assets/libraries/dataTables/css/jquery.dataTables.min.css">
+		<link rel="stylesheet" href="assets/libraries/dataTables/css/buttons.dataTables.min.css">
 
-		<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-		<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
-		<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
-		<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.colVis.min.js"></script>
-		<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+		<script src="assets/libraries/dataTables/js/jquery.dataTables.min.js"></script>
+		<script src="assets/libraries/dataTables/js/dataTables.buttons.min.js"></script>
+		<script src="assets/libraries/dataTables/js/buttons.colVis.min.js"></script>
+		<script src="assets/libraries/dataTables/js/buttons.html5.min.js"></script>
 
-		
 FILEDOC;
 			echo $datatables_source;
 		};
 ?>
-		
-		<!-- Twitter Bootstrap -->
-		<!-- Minified CSS -->
-		<!-- <link rel="stylesheet" href="assets/bootstrap/3.4.1/css/bootstrap.min.css"> -->
-
-		<!-- Optional theme -->
-		<!-- <link rel="stylesheet" href="assets/bootstrap/3.4.1/css/bootstrap-theme.min.css"> -->
-		<!-- Minified JavaScript for Bootstrap -->
-		<!-- <script src="assets/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
-
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+		<!-- Bootstrap CSS and JS -->
+		<link rel="stylesheet" href="assets/libraries/bootstrap/5.3.3/css/bootstrap.min.css">
+		<script src="assets/libraries/bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
 
 		<!-- Material Icons -->
-		<script src="//code.iconify.design/1/1.0.6/iconify.min.js"></script>
+		<script src="assets/libraries/iconify/1.0.6/iconify.min.js"></script>
 
 		<!-- Main CSS -->
-		<link rel="stylesheet" href="css/main.css?v=<?php echo time(); ?>">
+		<link rel="stylesheet" href="assets/css/main.css?v=<?php echo time(); ?>">
 
 	</head>
 	
